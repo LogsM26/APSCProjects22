093b62789b0b50e15d067eaf68dcbffde932a891
LogsM26 <112580528+LogsM26@users.noreply.github.com>
2022-09-20 14:48:07 -0700

    if

---
+++ Unit1/Fibonacci.java
@@ -0,0 +1,20 @@
+
+
+public class Fibonacci{
+
+  public static void main(String[] args){
+
+    int a = 1;
+    int b = 1;
+    int c = 1;
+
+    for(int = 0; i < 30; i++){
+
+      
+
+    }
+
+  }
+
+
+}
---
+++ Unit1/MultiplesOf3.java
@@ -0,0 +1,18 @@
+
+//DeMorgan's Laws
+
+
+public class MultiplesOf3{
+
+
+  public static void main(String[] args){
+
+    for(int i = 0; i<60; i+=3){
+
+      System.out.println(i);
+
+    }
+
+  }
+
+}
