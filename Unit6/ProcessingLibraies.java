d69ab0701241e42023610ba666f2a2dc88deed41
LogsM26 <112580528+LogsM26@users.noreply.github.com>
2022-10-17 13:47:20 -0700

    UNFINHES QHAT IS PAPLLETTEBFTHKREJWGNG

---
+++ Unit6/ProcessingLibraies.java
@@ -0,0 +1,45 @@
+
+
+/*
+
+NOTES FOR TERMINAL
+
+
++x
+  Execute
+
+./
+  run in current directory
+
+echo
+  echo something to the screen
+
+env
+  what the enviroment looks like
+  enviroment is basocally just a bunch of variables
+
+  $PATH
+    $ = print
+    Tells system where it can find files
+    We can alter it too
+      Make our own stuff
+
+ls -a
+
+CLASSPATH
+
+*/
+
+/*
+
+NOES FOR PROCESSING
+
+
+import processing.core.*;
+
+setup and draw and settings
+  Settings has size only
+
+extends
+
+*/
