076f45a9aaf9e1c4e20a4882c5105c2571c742c8
LogsM26 <112580528+LogsM26@users.noreply.github.com>
2022-10-21 10:20:27 -0700

    FIX IR HOW

--- Unit6/Snow.java
+++ Unit6/Snow.java
@@ -1,1 +1,67 @@
 
+
+
+import processing.core.*;
+import java.util.*;
+
+
+
+public class Snow extends PApplet{
+
+
+  private ArrayList<Snow> crystals;
+
+
+
+
+  public void settings(){
+
+    size(600, 600);
+  }
+
+
+
+  public void setup(){
+
+    crystals = new ArrayList<Snow>();
+    sprinkleSnow();
+  }
+
+
+  public void sprinkleSnow(){
+
+    PVector position = new PVector(random(0, width), 0);
+    PVector v = PVector.random2D();
+    v.setMag(random(1, 3));
+
+    float radius = random(2, 10);
+
+
+    Snow sphere = new SnowClass(this, position, v, radius);
+    crystals.add(sphere);
+
+
+    for(int i = 0; i < 150; i++){
+      sprinkleSnow();
+    }
+  }
+
+
+  public void draw(){
+
+    background(8, 125, 37);
+
+    for(Snow sphere: crystals){
+      sphere.display();
+    }
+  }
+
+
+
+
+  public static void main(String[] args){
+
+    PApplet.main("Snow");
+  }
+
+}
---
+++ Unit6/SnowClass.java
@@ -0,0 +1,44 @@
+
+
+import processing.core.*;
+
+
+public class SnowClass{
+
+
+  private PApplet p;
+
+  private PVector position;
+  private PVector v;
+
+  private float radius;
+
+
+
+  public SnowClass(PApplet p, PVector position, PVector v, float radius){
+
+    this.p = p;
+
+    this.position = position;
+    this.v = v;
+
+    this.radius = radius;
+  }
+
+
+
+  public void display(){
+
+    p.fill(255, 255, 255);
+    p.ellipse(position.x, position.y, radius*2, radius*2);
+
+    position.add(v);
+
+
+    if(position.y < 600){
+
+      position.y(0);
+    }
+  }
+
+}
