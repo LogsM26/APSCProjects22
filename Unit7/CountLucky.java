ca1ffa28c16dca41364196a6cd58d4adb1d91f85
LogsM26 <112580528+LogsM26@users.noreply.github.com>
2022-11-03 13:12:02 -0700

    Finsihed but ADD IN ENDS IN 7 SOMEHOW IDJBV

---
+++ Unit7/CountLucky.java
@@ -0,0 +1,71 @@
+
+
+public class CountLucky{
+
+
+
+  //multiple of 7
+  //ends in 7
+  //increase count by 1
+
+  public static int count7(int[] values){
+
+    int count = 0;
+
+    for(int value : values){
+
+      if(value%7 == 0){
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+
+
+  public static void testCount7(int[] values, int expected){
+
+    int result = count7(values);
+
+
+    //print the array's values
+    System.out.print("values: ");
+
+    for(int value : values){
+      System.out.print(value + " ");
+    }
+
+    //print expected and result
+    System.out.println("expected: " + expected + " result: " + result);
+
+
+    //if statement
+    if(expected == result){
+      System.out.println("W");
+    }else{
+      System.out.println("L");
+    }
+
+  }
+
+
+
+  public static void main(String[] args){
+
+    //create new values to make each value for the array
+    int[] values = new int[3];
+    values[0] = 7;
+    values[1] = 14;
+    values[2] = 21;
+
+    int[] values1 = new int[3];
+    values1[0] = 37;
+    values1[1] = 2;
+    values1[2] = 9;
+
+
+    testCount7(values, 3);
+    testCount7(values1, 0);
+  }
+}
---
+++ Unit7/SumOfSquaresArray.java
@@ -0,0 +1,8 @@
+
+
+public class SumOfSquaresArray{
+
+
+
+  public static 
+}
