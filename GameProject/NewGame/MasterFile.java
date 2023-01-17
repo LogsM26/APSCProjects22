45e48c896cf8cb75a804a693b5246bbb4b38dd7d
LogsM26 <112580528+LogsM26@users.noreply.github.com>
2023-01-17 08:29:44 -0800

    Gameproject stuff

--- GameProject/NewGame/MasterFile.java
+++ GameProject/NewGame/MasterFile.java
@@ -14,11 +14,15 @@ public class MasterFile extends PApplet{
 
 
   //Variables
+  public PApplet p;
 
     //Integers
 
+    //Floats
+
     //Strings
-    String gameState = "TITLE";
+    String gameState = "SELECT";
+
 
     //Images
       //Title
@@ -29,6 +33,11 @@ public class MasterFile extends PApplet{
       PImage p1;
       PImage p2;
       PImage p3;
+
+    //Fonts
+      //Main
+      PFont liquid;
+      int fontSize = 0;
 
 
 
@@ -43,6 +52,7 @@ public class MasterFile extends PApplet{
   public void setup(){
 
     //background(100,4,37);
+    liquid = createFont("DkLiquidEmbrace-YBvv.ttf", 75);
   }
 
 
@@ -78,9 +88,10 @@ public class MasterFile extends PApplet{
 
 
     //Main text
-    textSize(50);
+    textSize(75);
     fill(235, 198, 99);
-    text("Hunt for Stars", width/20, height/6+60);
+    textFont(liquid);
+    text("Hunt for Stars", width/20, height/6+75);
 
 
     //Buttons
@@ -115,7 +126,7 @@ public class MasterFile extends PApplet{
       //Planets
       p1 = loadImage("P1Earth.png");
       p1.resize(600, 600);
-      image(p1, width/2+25, height/2-100);
+      image(p1, width/2+25, height/2-125);
 
       p2 = loadImage("P2Mars.png");
       p2.resize(300, 300);
@@ -124,14 +135,63 @@ public class MasterFile extends PApplet{
       p3 = loadImage("P3Alge.png");
       p3.resize(200, 200);
       image(p3, width/2, height/20);
+
+
+    //Text
+    textFont(liquid);
+    textSize(30);
+    fill(103, 209, 214);
+    text("Return",  width/20-25, height-25);
   }
 
 
 
   public void mouseClicked(){
 
+    //During TITLE
+    if(gameState == "TITLE"){
+
+      if( (p.mouseX < width/20+15) && (p.mouseX > width/20)
+            && (p.mouseY < height-80) && (p.mouseY > height-100) ){
+
+              gameState = "SELECT";
+
+              System.out.println("fygvjhb");
+            }
+
+
+    //During SELECT
+    }else if(gameState == "SELECT"){
+
+      if( (p.mouseX < width/20-25) && (p.mouseX > width/20+5)
+            && (p.mouseY < height-35) && (p.mouseY > height+5) ){
+
+              gameState = "TITLE";
+
+              System.out.println("s");
+            }
+    }
+
+
+    //During P1
 
   }
+
+
+    /*
+    //Buttons
+    textSize(20);
+    fill(209, 73, 19);
+    text("Visit the Stars", width/20, height-100);
+
+    textSize(20);
+    fill(209, 73, 19);
+    text("Manual", width/20, height-75);
+
+    textSize(20);
+    fill(209, 73, 19);
+    text("Credits", width/20, height-50);
+    */
 
 
 
